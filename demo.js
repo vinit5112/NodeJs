c775ccb9e1edfc839608b66274f8ca1a00fb22d6
Tavde Vinit <92319422+vinit5112@users.noreply.github.com>
2022-09-08 11:54:59 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,13 @@
+console.log("welcome to Node");
+var a=10;
+var  b = 20;
+var c = a+b;
+console.log(`a value is ${a} and b value is ${b}`);
+console.log(`sum of a and b is ${c}`)
+
+if(c==30){
+    console.log("sum is 30");
+}else{
+    console.log("error");
+} 
+
---
+++ server.js
@@ -0,0 +1,15 @@
+var  http = require('http');
+var a = 10;
+var b = 20;
+var c = a+b;
+var msg = "sum is " + c;
+if(c==30){
+    msg += "<br/> c is 30"
+}else{
+    msg += "<br/> c is not 30"
+}
+http.createServer(function (req,res) {
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(msg);
+}).listen(3000);
+console.log("server is listening on http://127.0.0.1:3000");
